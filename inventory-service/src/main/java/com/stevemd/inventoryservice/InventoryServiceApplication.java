7fd5124d54248a5d8ca7978e0bf832b944b8d0dc
MuindiStephen <stephenmuindi241@gmail.com>
2024-03-20 01:09:25 +0300

    discovery server

--- inventory-service/src/main/java/com/stevemd/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/stevemd/inventoryservice/InventoryServiceApplication.java
@@ -5,10 +5,12 @@ import com.stevemd.inventoryservice.util.DataLoader;
 import lombok.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @Getter
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
