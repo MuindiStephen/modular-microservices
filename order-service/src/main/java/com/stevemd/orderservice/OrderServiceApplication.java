76373434a170fc7ea1633f0e4cddf01a30040827
MuindiStephen <stephenmuindi241@gmail.com>
2024-03-20 09:38:15 +0300

    rebuit project: module remove and resolve dependencies

--- order-service/src/main/java/com/stevemd/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/stevemd/orderservice/OrderServiceApplication.java
@@ -2,10 +2,9 @@ package com.stevemd.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- product-service/src/main/java/com/stevemd/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/stevemd/productservice/ProductServiceApplication.java
@@ -3,11 +3,10 @@ package com.stevemd.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 @EntityScan
-@EnableDiscoveryClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
